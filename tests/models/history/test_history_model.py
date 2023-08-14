3a05a2ac771f85a23427dc748d8a7fcb13fac3be
Vitor Wagner <vitormwagner@gmail.com>
2023-08-14 17:16:24 -0300

    Test: :test_tube: Added tests for `list_as_json` function of `HistoryModel`

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,9 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
-def test_request_history():
-    raise NotImplementedError
+def test_request_history(prepare_base):
+    history = json.loads(HistoryModel.list_as_json())
+    assert history[0]['text_to_translate'] == 'Hello, I like videogame'
+    assert history[1]['text_to_translate'] == 'Do you love music?'
