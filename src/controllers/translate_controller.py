685faf0e411a1632f50e72e6eea8c84856429f9e
Vitor Wagner <vitormwagner@gmail.com>
2023-08-14 15:58:14 -0300

    Feat: :sparkles: Added translation function to `translate_controllet.py`

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,16 +1,32 @@
-from flask import Blueprint, render_template
-# from deep_translator import GoogleTranslator
-# from models.language_model import LanguageModel
+from flask import Blueprint, render_template, request
+from deep_translator import GoogleTranslator
+from models.language_model import LanguageModel
 # from models.history_model import HistoryModel
 
 
 translate_controller = Blueprint("translate_controller", __name__)
 
+def _get_all_languages():
+    return LanguageModel.list_dicts()
+
 
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    text_to_translate=request.form.get("text-to-translate") if request.method == "POST" else ""
+    translate_from=request.form.get("translate-from") if request.method == "POST" else "en"
+    translate_to=request.form.get("translate-to") if request.method == "POST" else "pt"
+    translated=GoogleTranslator(source=translate_from, target=translate_to).translate(text_to_translate) if request.method == "POST" else "Tradução"
+    
+
+    return render_template(
+        "index.html", 
+        languages=_get_all_languages(),
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated
+    )
 
 
 # Req. 6
