bc265c96e9a1359f098f36fc63126f580948b6c5
Vitor Wagner <vitormwagner@gmail.com>
2023-08-16 16:31:35 -0300

    Test: Tests for history delete function

--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,6 +1,32 @@
-# from src.models.history_model import HistoryModel
-# from src.models.user_model import UserModel
+from src.models.history_model import HistoryModel
+from src.models.user_model import UserModel
 
 
 def test_history_delete(app_test):
-    raise NotImplementedError
+    history = HistoryModel(
+        {
+            "text_to_translate": "Hello",
+            "translate_from": "en",
+            "translate_to": "pt",
+            "translated": "Olá",
+        }
+    )
+    history.save()
+
+    user = UserModel(
+        {
+            "name": "test",
+            "token": "777",
+        }
+    )
+    user.save()
+
+    app_test.delete(
+        f"/admin/history/{history.data['_id']}",
+        headers={
+            "Authorization": "777",
+            "User": "test",
+        },
+    )
+
+    assert HistoryModel.find_one({"_id": history.data["_id"]}) is None
